8050a933905c0c6ce25b60a9dc4109bb6508bfd8
Roberto Corrales <roberto.corrales@walmart.com>
2023-08-05 15:26:39 -0600

    unit tests for login coordinator

---
+++ CardFortressTests/Login/LoginCoordinatorTests.swift
@@ -0,0 +1,59 @@
+//
+//  LoginCoordinatorTests.swift
+//  CardFortressTests
+//
+//  Created by Roberto Corrales on 8/5/23.
+//
+
+import XCTest
+@testable import CardFortress
+
+final class LoginCoordinatorTests: XCTestCase {
+
+    var coordinator: LoginCoordinator!
+    var navigationController: UINavigationController!
+    var factory: LoginFactoryMock!
+    
+    override func setUp() {
+        super.setUp()
+        factory = LoginFactoryMock()
+        navigationController = UINavigationController()
+        coordinator = .init(
+            factory: factory,
+            navigationController: navigationController,
+            authenticationAPI: AuthenticationAPIMock())
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        coordinator = nil
+        navigationController = nil
+        factory = nil
+    }
+    
+    func test_startCoordinator() {
+        //given
+        let loginViewController = factory.loginViewController
+        //when
+        XCTAssertEqual(navigationController.viewControllers.count, 0)
+        XCTAssertNil(navigationController.topViewController)
+        coordinator.start()
+        //then
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+        XCTAssertEqual(navigationController.topViewController, loginViewController)
+    }
+    
+    @MainActor
+    func test_login() async {
+        //given
+        let loginViewController = factory.loginViewController
+        let emailPassword = "user"
+        let password = "password"
+        //when
+        coordinator.start()
+        let result = await coordinator.login(email: emailPassword, password: password)
+        //then
+        XCTAssertEqual(result, .success)
+    }
+
+}
