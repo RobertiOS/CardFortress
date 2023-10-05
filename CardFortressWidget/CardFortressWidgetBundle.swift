5c6b349068be25f8e2c6aa871e712fd0f95ad037
Robert <robert.ios.developer@gmail.com>
2023-10-04 23:06:44 -0600

    add widget extension

---
+++ CardFortressWidget/CardFortressWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  CardFortressWidgetBundle.swift
+//  CardFortressWidget
+//
+//  Created by Roberto Corrales on 10/4/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CardFortressWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CardFortressWidget()
+        CardFortressWidgetLiveActivity()
+    }
+}
