563c88dec060c39b2874c4c4d193f74a5f090c85
Robert <robert.ios.developer@gmail.com>
2023-10-04 22:29:18 -0600

    update tests and API mocks

--- CardFortress/Resources/APIMocks/DataStorageAPIMock.swift
+++ CardFortress/Resources/APIMocks/DataStorageAPIMock.swift
@@ -8,12 +8,11 @@
 import Foundation
 
 final class DataStorageAPIMock: DataStorageAPI {
-    func storeUserData(userUid: String, userData: UserData) async -> StorageResult {
-        return .success
-    }
-
-    func getUserData(uid: String) async -> Result<UserData?, Error> {
-        return .success(.init(name: "Juan", lastName: "Perez"))
+    func getUserData(uid: String) async throws -> UserData {
+        .init(name: "Juan", lastName: "Perez")
     }
     
+    func storeUserData(userUid: String, userData: UserData) async throws {
+       
+    }
 }
