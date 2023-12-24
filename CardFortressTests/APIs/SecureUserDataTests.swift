5768128dac0d7b3904c3a31d2324ef964a02bddc
Robert <robert.ios.developer@gmail.com>
2023-12-23 19:21:39 -0600

    increase coverage

--- CardFortressTests/APIs/SecureUserDataTests.swift
+++ CardFortressTests/APIs/SecureUserDataTests.swift
@@ -13,17 +13,34 @@ final class SecureUserDataTests: XCTestCase {
     func test_storeUserCredentials() async {
         // Given
         let secureUserData = SecureUserData(server: .testServer)
+        
+        /// delete account if it exists
+        secureUserData.deleteAccount()
+        
+        let getCredentials3 = await secureUserData.getUserCredentials()
+        guard case .failure(error: _) = getCredentials3 else { return XCTFail("User credentials not expected at this point") }
+        
         let mockloginInfo = LoginInfo(userName: "user", password: "1234")
+        let mockloginInfo2 = LoginInfo(userName: "userModified", password: "1234")
         // When
         let result = await secureUserData.storeUserCredentials(userData: mockloginInfo)
         // Then
-        guard case .success(let loginInfo) = result else { return }
+        guard case .success(let loginInfo) = result else { return XCTFail("User credentials expected at this point") }
         XCTAssertEqual(loginInfo, mockloginInfo)
         
         //when
         let getCredentials = await secureUserData.getUserCredentials()
-        guard case .success(let loginInfo2) = getCredentials else { return }
+        guard case .success(let loginInfo2) = getCredentials else { return XCTFail("User credentials expected at this point") }
+        
+        // then
         XCTAssertEqual(loginInfo2, mockloginInfo)
+        
+        // When
+        let result2 = await secureUserData.storeUserCredentials(userData: mockloginInfo2)
+        // Then
+        guard case .success(let loginInfo2) = result2 else { return XCTFail("User credentials expected at this point")  }
+        
+        XCTAssertEqual(loginInfo2, mockloginInfo2)
     }
 
 }
