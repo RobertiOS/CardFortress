672179780c4781893d196e7501706045e1ce3a50
Robert <robert.ios.developer@gmail.com>
2024-03-11 09:54:57 -0600

    update domain depository

--- CardFortress/Modules/CFHelper/Sources/FeatureModules/CFAPIs/APIs/SecureStore/Domain/CreditCardRepository.swift
+++ CardFortress/Modules/CFHelper/Sources/FeatureModules/CFAPIs/APIs/SecureStore/Domain/CreditCardRepository.swift
@@ -10,35 +10,23 @@ import Foundation
 public protocol CreditCardRepository {
     /// Removes credit cards from the repository
     /// - Returns:returns an secure store result
-    @discardableResult
-    func removeAllCreditCards() async throws -> CreditCardsRepositoryResult
+    func removeAllCreditCards() async throws
     /// Adds a credit card to the  the repository
     /// - Parameter card: the card to be added
-    /// - Returns: returns an secure store result
-    ///
-    @discardableResult
     /// Removes a credit card
     /// - Parameter id: The id of the credit card
-    /// - Returns: a result indicating if the card was deleted succesfuly
-    func removeCreditCard(id: UUID) async throws -> CreditCardsRepositoryResult
+    func removeCreditCard(id: UUID) async throws
     /// Adds a credit card to the  the repository
     /// - Parameter card: the card to be added
-    /// - Returns: returns an secure store result
-    @discardableResult
-    func addCreditCard(_ creditCard: SecureStoreCreditCard) async throws -> CreditCardsRepositoryResult
+    func addCreditCard(_ creditCard: DomainCreditCard) async throws
     /// Returns a credit card  from the repository
     /// - Parameter identifier: The identifier of the credit card (UUID)
-    /// - Returns: optional credit card
-    func getCreditCard(identifier: UUID) async throws -> DomainCreditCard?
+    /// - Returns: a credit card
+    func getCreditCard(identifier: UUID) async throws -> DomainCreditCard
     /// Returns all credit cards from the repository
     /// - Returns: Array of credit cards
     func getAllCreditCards() async throws -> [DomainCreditCard]
     /// Returns the Favorite credit card from the repository
-    /// - Returns: An optional credit
-    func getFavoriteCreditCard() async -> DomainCreditCard?
-}
-
-public enum CreditCardsRepositoryResult {
-    case success
-    case failure
+    /// - Returns: A credit card marked as favorite
+    func getFavoriteCreditCard() async throws -> DomainCreditCard?
 }
